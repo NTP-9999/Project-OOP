3da197c201a47b856f87bf858e0c69171f3b271e
HuTaoDaYoo!!! <japankyu@gmail.com>
2025-10-01 20:59:12 +0700

    Update PlaceableStructureItem.cs

--- Assets/Script/Item/PlaceableStructureItem.cs
+++ Assets/Script/Item/PlaceableStructureItem.cs
@@ -8,8 +8,8 @@ public class PlaceableStructureSO : ItemSO
     [SerializeField] private List<RequireItem> requireItem;
     public RequireItem[] RequireItems => requireItem.ToArray();
     
-[System.Serializable]
-public class RequireItem
+    [System.Serializable]
+    public class RequireItem
     {
         public ItemSO item;
         public int amount;
